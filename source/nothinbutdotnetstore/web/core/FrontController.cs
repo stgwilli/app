d9b39d289158b0d3fbd088a9f684d639a49ea2d9
swilliams <swilliams@daxko.com>
2011-09-20 15:09:23 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -2,9 +2,16 @@
 {
     public class FrontController :IProcessRequests
     {
+        IFindCommands command_finder;
+
+        public FrontController(IFindCommands command_finder)
+        {
+            this.command_finder = command_finder;
+        }
+
         public void process(IContainRequestInformation request)
         {
-            throw new System.NotImplementedException();
+            command_finder.get_the_command_that_can_process(request).process(request);
         }
     }
 }
